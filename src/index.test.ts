f2efe0ab1ca4e1767750ee093bf909e256f99fa1
Emma Goodliffe <emma@piemag.com>
2021-06-04 15:36:57 +0100

    ready for fuctions

--- src/index.test.ts
+++ src/index.test.ts
@@ -7,12 +7,12 @@ import {
   Int,
   Mult,
   Permute,
-  PiecewiseFunction,
   Power,
   PowerFactorisation,
   SigmaSummation,
   Sub,
   Summation,
+  Variable,
 } from "./index";
 
 test("new", () => {
@@ -43,53 +43,51 @@ test("new", () => {
   expect(choose.simplify().compute()).toBe((5 * 4) / 2);
   const permute = new Permute(5, 2);
   expect(permute.simplify().compute()).toBe(5 * 4);
-  const sigma = new SigmaSummation(
-    two,
-    three,
-    x => new Fraction(new Int(x), two),
-  );
+  const sigma = new SigmaSummation(two, three, x => new Fraction(x, two));
   expect(sigma.simplify().compute()).toBe(5 / 2);
-  const piecewise = (x: number) =>
-    new PiecewiseFunction(
-      [{ expression: () => new Int(100), condition: ([x]) => x === 2 }],
-      [x],
-      ([x]) => new Int(x),
-    );
-  expect(piecewise(2).simplify().compute()).toBe(100);
-  expect(piecewise(3).simplify().compute()).toBe(3);
 });
 
 // TODO: from
 
 test("heads/tails", () => {
-  const f = (x: number, d: number) =>
-    new PiecewiseFunction(
-      [
-        {
-          expression([x, d]) {
-            const a = new Choose(x, x - d);
-            const b = new Choose(x - 1, x - d - 1);
-            return new Sub(a, b);
-          },
-          condition: ([x, d]) => x >= 1 && x - d >= 1,
-        },
-      ],
-      [x, d],
-      () => new Int(1),
-    );
+  const f = (x: number, d: number) => {
+    if (x < 1 || x - d < 1) {
+      return new Int(1);
+    } else {
+      const a = new Choose(x, x - d);
+      const b = new Choose(x - 1, x - d - 1);
+      return new Sub(a, b);
+    }
+  };
 
-  const term = (x: number, m: number, p: number) => {
-    const num = f(x, m - p);
-    const den = new Power(new Int(2), new Int(x));
+  const term = (
+    x: Int | Variable,
+    m: number,
+    p: number,
+    indexSymbol: string,
+  ) => {
+    if (x instanceof Variable) {
+      const num = new Variable(`f(${indexSymbol}, ${m} - ${p})`);
+      const den = new Power(new Int(2), new Variable(indexSymbol));
+      return new Fraction(num, den);
+    }
+    const num = f(x.compute(), m - p);
+    const den = new Power(new Int(2), x);
     return new Fraction(num, den);
   };
 
-  const sum = (m: number, p: number, q: number) => {
+  const sum = (m: number, p: number, q: number, indexSymbol: string) => {
     const lowerBound = new Int(m - p);
     const upperBound = new Int(2 * m - p - q - 1);
-    return new SigmaSummation(lowerBound, upperBound, x => term(x, m, p));
+    return new SigmaSummation(
+      lowerBound,
+      upperBound,
+      x => term(x, m, p, indexSymbol),
+      indexSymbol,
+    );
   };
 
-  const surd = sum(5, 3, 2);
-  console.log(`${surd.katex()} => ${surd.simplify().katex()}`);
+  const surd = sum(5, 3, 2, "i");
+  expect(surd.simplify().compute()).toBe(11 / 16);
+  console.log(`${surd.katex()} = ${surd.simplify().katex()}`);
 });
